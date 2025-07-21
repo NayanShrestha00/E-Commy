73a9c50dcec96fbc54c0bac03f92b87a79baa853
NayanShrestha00 <nayanshrestha00@gmail.com>
2025-07-21 13:26:42 +0545

    second commit

---
+++ components/post/postcart.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class postcart extends Component {
+  render() {
+    return (
+      <View>
+        <Text>postcart</Text>
+      </View>
+    )
+  }
+}
+
+export default postcart
---
+++ components/product/productDetials.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class productDetials extends Component {
+  render() {
+    return (
+      <View>
+        <Text>productDetials</Text>
+      </View>
+    )
+  }
+}
+
+export default productDetials
